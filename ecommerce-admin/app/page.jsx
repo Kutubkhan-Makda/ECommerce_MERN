b55beeb5d0e206cddbbf95126d23f10220ffde7e
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-10-27 15:09:51 +0530

    first commit

---
+++ ecommerce-admin/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='bg-blue-900 h-screen w-screen flex items-center'>
+      <div className='text-center w-full'>
+        <button className='bg-white p-2'>Login with Google</button>
+      </div>
+    </div>
+  )
+}
