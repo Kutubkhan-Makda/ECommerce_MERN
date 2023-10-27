ed1c9456887059dda8386f1d4ad90d6a3774725f
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-10-27 15:13:44 +0530

    day-1

--- ecommerce-admin/app/page.jsx
+++ ecommerce-admin/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='bg-blue-900 h-screen w-screen flex items-center'>
       <div className='text-center w-full'>
-        <button className='bg-white p-2'>Login with Google</button>
+        <button className='bg-white p-2 rounded-md'>Login with Google</button>
       </div>
     </div>
   )
